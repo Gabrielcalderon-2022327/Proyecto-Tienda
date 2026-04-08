35c39ab2c4a80c8f2ab5fb16839090d335c88b85
Gabrielcalderon-2022327 <gcalderon-2022327>
2026-04-08 11:35:11 -0600

    feat: Se agrego funcion para mostrar el username en Productos

--- src/main/java/com/gabrielcalderon/Tienda/Controller/ProductosView.java
+++ src/main/java/com/gabrielcalderon/Tienda/Controller/ProductosView.java
@@ -2,15 +2,17 @@ package com.gabrielcalderon.Tienda.Controller;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductosView {
     @GetMapping("/productos")
-    public String cargarProductos(HttpSession session){
+    public String cargarProductos(HttpSession session, Model model){
         if(session.getAttribute("usuarioLogueado") == null){
             return "redirect:/login";
         }
+        model.addAttribute("username", session.getAttribute("username"));
         return "Productos";
     }
 }
