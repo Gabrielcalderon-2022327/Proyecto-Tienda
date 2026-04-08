001db26679e7296f97db22ce834919419bd3b0ff
Gabrielcalderon-2022327 <gcalderon-2022327>
2026-04-08 08:37:29 -0600

    feat: Se añadio comprobación para cargar el index

--- src/main/java/com/gabrielcalderon/Tienda/Controller/IndexController.java
+++ src/main/java/com/gabrielcalderon/Tienda/Controller/IndexController.java
@@ -1,8 +1,19 @@
 package com.gabrielcalderon.Tienda.Controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    @GetMapping("/index")
+    public String mostrarIndex(HttpSession session){
+        //Validar Sesion
+        if(session.getAttribute("usuarioLogueado") == null){
+            return "redirect:/login";
+        }
+        return "index";
+    }
+
     
 }
