51b60aab47713c82939f99d1acc5426bc06aceb4
Gabrielcalderon-2022327 <gcalderon-2022327>
2026-04-08 09:22:44 -0600

    feat: Se agrego el controlador de la vista de Clientes

---
+++ src/main/java/com/gabrielcalderon/Tienda/Controller/ClientesView.java
@@ -0,0 +1,16 @@
+package com.gabrielcalderon.Tienda.Controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientesView {
+    @GetMapping("/clientes")
+    public String cargarClientes(HttpSession session){
+        if(session.getAttribute("usuarioLogueado") == null){
+            return "redirect:/login";
+        }
+        return "clientes";
+    }
+}
