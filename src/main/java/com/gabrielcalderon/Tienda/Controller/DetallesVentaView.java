7f2582741d766c0179b8c3adb3e82f1e7871456a
Gabrielcalderon-2022327 <gcalderon-2022327>
2026-04-08 11:34:54 -0600

    feat: Se agrego funcion para mostrar el username en DetalleVenta

--- src/main/java/com/gabrielcalderon/Tienda/Controller/DetallesVentaView.java
+++ src/main/java/com/gabrielcalderon/Tienda/Controller/DetallesVentaView.java
@@ -2,15 +2,17 @@ package com.gabrielcalderon.Tienda.Controller;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DetallesVentaView {
     @GetMapping("/detalles")
-    public String cargarDetalles(HttpSession session){
+    public String cargarDetalles(HttpSession session, Model model){
         if(session.getAttribute("usuarioLogueado") == null){
             return "redirect:/login";
         }
+        model.addAttribute("username", session.getAttribute("username"));
         return "DetalleVenta";
     }
 }
