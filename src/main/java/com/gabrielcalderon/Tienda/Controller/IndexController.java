d6eb9f26b33d9265980505bff103621f4d045eef
Gabrielcalderon-2022327 <gcalderon-2022327>
2026-04-08 11:35:04 -0600

    feat: Se agrego funcion para mostrar el username en Index

--- src/main/java/com/gabrielcalderon/Tienda/Controller/IndexController.java
+++ src/main/java/com/gabrielcalderon/Tienda/Controller/IndexController.java
@@ -2,18 +2,20 @@ package com.gabrielcalderon.Tienda.Controller;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
     @GetMapping("/index")
-    public String mostrarIndex(HttpSession session){
+    public String mostrarIndex(HttpSession session, Model model){
         //Validar Sesion
         if(session.getAttribute("usuarioLogueado") == null){
             return "redirect:/login";
         }
+        model.addAttribute("username", session.getAttribute("username"));
         return "index";
     }
 
-    
+
 }
