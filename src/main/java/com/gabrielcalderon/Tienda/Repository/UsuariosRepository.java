839c28e1dc7f36043a708ce677710748f4b2ca1e
Gabrielcalderon-2022327 <gcalderon-2022327>
2026-04-14 11:32:10 -0600

    feat: Se añadio la funcion de buscar por usuario y por email.

--- src/main/java/com/gabrielcalderon/Tienda/Repository/UsuariosRepository.java
+++ src/main/java/com/gabrielcalderon/Tienda/Repository/UsuariosRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuariosRepository extends JpaRepository<Usuarios, Integer> {
+    Usuarios findByUsername(String username);
+    Usuarios findByEmail(String email);
 }
